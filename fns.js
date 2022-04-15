b6d3c433828987e5aebda30546abec2bff65fd8a
agustindigiu <agustindigiu@gmail.com>
2022-04-15 19:00:01 -0300

    Agrego funcion multiplicar y division

--- fns.js
+++ fns.js
@@ -12,11 +12,22 @@ function sum(a, b) {
 
 // Completar estas funciones
 
-function res(a, b) {}
+function res(a, b) {
+    
+}
 
-function mul(a, b) {}
+function mul(a, b) {
+    return a * b;
+}
 
 function div(a, b) {
+    if (b==0){
+        return "Error: div by 0";
+    }
+    else{
+        return a / b;
+    }
+    
     // Tener en cuenta division por 0
     // Si b es 0 retornar "Error: div by 0"
 }
